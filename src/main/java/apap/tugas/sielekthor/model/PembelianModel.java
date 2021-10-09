865693dc8b3408909e1bc10e94b3ea7d41e51284
danielsyahputra13 <dnlshp@gmail.com>
2021-10-09 22:45:43 +0700

    feat: implement Pembelian model

---
+++ src/main/java/apap/tugas/sielekthor/model/PembelianModel.java
@@ -0,0 +1,43 @@
+package apap.tugas.sielekthor.model;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "pembelian")
+public class PembelianModel implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPembelian;
+
+    @NotNull
+    @Size(max = 30)
+    @Column(name = "nama_admin", nullable = false)
+    private String namaAdmin;
+
+    @NotNull
+    @Column(nullable = false)
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private Date tanggalPembelian;
+
+    @NotNull
+    @Column(name = "total_pembelian", nullable = false)
+    private Integer totalPembelian;
+
+    @NotNull
+    @Column(name = "is_cash", nullable = false)
+    private Integer isCash;
+}
