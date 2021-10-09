01271b1667110d42b38e71bc9e4fd33342619e6d
danielsyahputra13 <dnlshp@gmail.com>
2021-10-09 14:00:50 +0700

    feat: implement Tipe model

--- src/main/java/apap/tugas/sielekthor/model/TipeModel.java
+++ src/main/java/apap/tugas/sielekthor/model/TipeModel.java
@@ -1,4 +1,34 @@
 package apap.tugas.sielekthor.model;
 
-public class TipeModel {
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "tipe")
+public class TipeModel implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idTipe;
+
+    @NotNull
+    @Size(max = 30)
+    @Column(name = "nama_tipe", nullable = false)
+    private String namaTipe;
+
+    @NotNull
+    @Size(max = 30)
+    @Column(name = "deskripsi_tipe", nullable = false)
+    private String deskripsiTipe;
+
 }
