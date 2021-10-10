b63534e626520c12bd62bda1d4db0c8c63bfaf5d
danielsyahputra13 <dnlshp@gmail.com>
2021-10-10 18:09:46 +0700

    feat: implement add barang

--- src/main/java/apap/tugas/sielekthor/controller/BarangController.java
+++ src/main/java/apap/tugas/sielekthor/controller/BarangController.java
@@ -1,4 +1,42 @@
 package apap.tugas.sielekthor.controller;
 
+import apap.tugas.sielekthor.model.BarangModel;
+import apap.tugas.sielekthor.model.TipeModel;
+import apap.tugas.sielekthor.service.BarangService;
+import apap.tugas.sielekthor.service.TipeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
 public class BarangController {
+    @Qualifier("barangServiceImpl")
+
+    @Autowired
+    private BarangService barangService;
+
+    @Autowired
+    private TipeService tipeService;
+
+    @GetMapping("/barang/add")
+    public String addBarangFormPage(Model model) {
+        BarangModel barang = new BarangModel();
+        List<TipeModel> listTipe = tipeService.getListTipe();
+        model.addAttribute("barang", barang);
+        model.addAttribute("listTipe", listTipe);
+        return "form-add-barang";
+    }
+
+    @PostMapping("/barang/add")
+    public String addAgensiSubmitPage(@ModelAttribute BarangModel barang, Model model) {
+        barangService.addBarang(barang);
+        model.addAttribute("pesan", "Barang dengan kode barang " + barang.getKodeBarang() + " berhasil ditambahkan.");
+        return "info";
+    }
 }
--- src/main/java/apap/tugas/sielekthor/repository/TipeDb.java
+++ src/main/java/apap/tugas/sielekthor/repository/TipeDb.java
@@ -3,6 +3,7 @@ package apap.tugas.sielekthor.repository;
 import apap.tugas.sielekthor.model.TipeModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TipeDb extends JpaRepository<TipeModel, Long> {
